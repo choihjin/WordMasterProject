01b38f85d4ac552429e5a03cb7b6c3a162d34bba
choehyeogjin <21900772@handong.ac.kr>
2022-09-06 01:00:36 +0900

    create + read 구현

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -10,7 +10,7 @@ public class WordManager {
         wordCRUD = new WordCRUD(s);
     }
     public int selectMenu() {
-        System.out.print("*** 영단어 마스터 ***/n"
+        System.out.print("*** 영단어 마스터 ***\n"
                 + "*****************\n"
                 + "1. 모든 단어 보기\n"
                 + "2. 수준별 단어 보기\n"
@@ -35,5 +35,6 @@ public class WordManager {
                 wordCRUD.addWord();
             }
         }
+        System.out.println("프로그램 종료! 다음에 만나요~");
     }
 }
